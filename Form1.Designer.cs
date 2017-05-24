69f920895338ebbc61f591e5687f8963da278e5f
student <student@NANO-17>
2017-05-24 09:40:46 +0200

    Dodany przycisk zamknij i jeden bez funkcji, wczesniej powiekszono okno

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.przycisk = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // przycisk
+            // 
+            this.przycisk.Location = new System.Drawing.Point(12, 12);
+            this.przycisk.Name = "przycisk";
+            this.przycisk.Size = new System.Drawing.Size(75, 23);
+            this.przycisk.TabIndex = 0;
+            this.przycisk.Text = "Zamknij";
+            this.przycisk.UseVisualStyleBackColor = true;
+            this.przycisk.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 234);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.przycisk);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button przycisk;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace KlientTCP
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
